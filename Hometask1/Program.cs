631f6f0413e336cd217b8a8960da1e210120913d
Maxim <lyskomax2@mail.ru>
2024-01-17 20:40:57 +0300

    Add Hometask1

---
+++ Hometask1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого четная.
+
+// Функция определения четности суммы цифр заданного числа
+bool IsEven(int n)
+{
+    int sum = 0;
+    int digitNumber = 0;
+    while (n > 0)
+    {
+        digitNumber = n % 10;
+        n /= 10;
+        sum += digitNumber;
+    }
+    // Console.WriteLine(sum);
+    if (sum % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool workProgramm = true;
+while (workProgramm)
+{
+    Console.WriteLine("Введите целое число или 'q' для завершения работы программы");
+    string input = Console.ReadLine();
+
+    if (input == Convert.ToString("q"))
+    {
+        Console.WriteLine("Введено q, завершение работы программы");
+        workProgramm = false;
+    }
+    int number = Convert.ToInt32(input);
+    bool even = IsEven(number);
+    if (even == true)
+    {
+        Console.WriteLine($"Сумма цифр введенного числа {number} - четная, завершение работы программы");
+        workProgramm = false;
+    }
+}
